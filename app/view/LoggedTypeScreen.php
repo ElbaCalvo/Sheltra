e0733bedfebb0dbd5e90f77a203b007193edbcdf
ElbaCalvo <calvogarciaelba@gmail.com>
2025-04-23 16:55:07 +0200

    Creado el .css y .php de la pantalla de tipos de animales logeada (parte visual)

---
+++ app/view/LoggedTypeScreen.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sheltra - Inicio</title>
+    <link rel="stylesheet" href="css/LoggedTypeScreen.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-content">
+            <img src="../../img/sheltra-logo.png" alt="Sheltra" class="logo">
+            <div class="user-info">
+                <img src="../../img/favorites-icon.png" alt="Favorites" class="favorites-icon">
+                <img src="../../img/user-icon.png" alt="User" class="user-icon">
+                <span>NombreUsuario</span>
+            </div>
+        </div>
+    </header>
+
+    <div class="animal-type-header">
+        <img src="../../img/birds-banner.jpg" alt="Birds" class="banner-image">
+    </div>
+    
+    <h2>Tipo</h2>
+
+    <div class="animals-grid">
+        <div class="animal-card">
+            <img src="../../img/placeholder.png" alt="Animal" class="animal-image">
+            <h3>Nombre del animal</h3>
+            <p>Tipo de animal</p>
+            <div class="card-footer">
+                <button class="view-more">Ver más</button>
+                <img src="../../img/empty-like.png" alt="Paw" class="paw-icon">
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
